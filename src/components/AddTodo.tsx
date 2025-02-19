df9d557b05e8f9705eb878e3d0d65cc802daf332
Sagar Gautam <sagargautam389@gmail.com>
2025-02-19 14:33:55 +0545

    mediaQuery

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,34 +1,33 @@
 import { FormEvent, useState } from "react";
 import { useTodos } from "../store/TodosProvider";
-import './AddTodo.css'
-
+import "./AddTodo.css";
 
 function AddTodo() {
   const [todo, setTodo] = useState("");
   const [error, setError] = useState(""); // State to store error message
-  const{handleAddTodo} =useTodos();
+  const { handleAddTodo } = useTodos();
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if(todo){
-      setError('')
+    if (todo) {
+      setError("");
       handleAddTodo(todo);
-    }else{
-      setError("Please enter some message !")
+    } else {
+      setError("Please enter some message !");
     }
     setTodo("");
   };
 
   return (
     <>
-     {error && <p className="errorMsg">{error}</p>}
-    <form onSubmit={handleFormSubmit}>
-      <input
-        type="text"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-      />
-      <button type="submit">Add</button>
-    </form>
+      {error && <p className="errorMsg">{error}</p>}
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
     </>
   );
 }
